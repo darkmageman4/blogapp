c9570ea59b9bcd29a992f484d9e0df610804b368
darkmageman4 <darkmageman4@yahoo.com>
2021-08-11 12:03:48 -0400

    blogapp examples

---
+++ src/main/java/com/codeup/blogapp/HelloController.java
@@ -0,0 +1,28 @@
+package com.codeup.blogapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello from Spring";
+    }
+
+    @GetMapping("/person")
+    @ResponseBody
+    public Person getPerson(){
+        return new Person ("Casey","Bum");
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+}
